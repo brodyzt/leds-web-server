04b0fa39c5402274b6cf914db078b4b2d6d64dde
brodyzt <brodyzt@gmail.com>
2017-06-03 13:15:27 -0400

    Add Flash

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, render_template, request
 from flask_restful import Resource, Api
 import thread
 import pigpio
+import time
 
 RED_PIN = 15
 GREEN_PIN = 18
@@ -13,6 +14,11 @@ pi = pigpio.pi()
 
 app = Flask(__name__)
 api = Api(app)
+
+mode = "static"
+
+def setPin(pin, brightness):
+    pi.set_PWM_dutycycle(pin, brightness)
 
 class Color():
     def __init__(self, name="", red=0, green=0, blue=0):
@@ -26,12 +32,31 @@ myColors = {"color": myColor}
 
 @app.route("/setColor", methods=['PUT'])
 def put():
-    pi.set_PWM_dutycycle(RED_PIN, request.get_json()['red'])
-    pi.set_PWM_dutycycle(GREEN_PIN, request.get_json()['green'])
-    pi.set_PWM_dutycycle(BLUE_PIN, request.get_json()['blue'])
+    mode = "static"
+    setPin(RED_PIN, request.get_json()['red'])
+    setPin(GREEN_PIN, request.get_json()['green'])
+    setPin(BLUE_PIN, request.get_json()['blue'])
     print("Color Set")
     return "Complete"
 
+@app.route("/flash", methods=['PUT'])
+def flash():
+    state = "ON"
+    mode = "flash"
+    while mode == "flash":
+        if state == "OFF":
+            setPin(RED_PIN, 255)
+            setPin(GREEN_PIN, 255)
+            setPin(BLUE_PIN, 255)
+            state = "ON"
+
+        else:
+            setPin(RED_PIN, 0)
+            setPin(GREEN_PIN, 0)
+            setPin(BLUE_PIN, 0)
+            state = "OFF"
+        time.sleep(1)
+    return "Flash Complete"
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
