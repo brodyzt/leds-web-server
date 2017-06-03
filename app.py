f8f6eefd946a76004666e8f86f529914cad57562
brodyzt <brodyzt@gmail.com>
2017-06-03 12:01:30 -0400

    Initial

--- app.py
+++ app.py
@@ -3,6 +3,16 @@
 from flask import Flask, jsonify, render_template, request
 from flask_restful import Resource, Api
 import thread
+import RPi.GPIO as GPIO
+
+RED_PIN = 15
+GREEN_PIN = 18
+BLUE_Pin = 14
+
+channel_list = [RED_PIN, GREEN_PIN, BLUE_Pin]
+
+GPIO.setup(channel_list, GPIO.OUT)
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,42 +24,17 @@ class Color():
         self.green = str(green)
         self.blue = str(blue)
 
-    def json(self):
-        return color_to_json(self)
-
-purple = Color("Purple", 255,0,255)
-turquoise = Color("Turquoise", 0,255,255)
-
-def color_to_json(color):
-    return {
-        color.name: {
-            "Red": color.red,
-            "Green": color.green,
-            "Blue": color.blue
-        }
-    }
-
 myColor = Color("color", 100, 100, 100)
 myColors = {"color": myColor}
 
-
+@app.route("/setColor", methods=['PUT'])
 class ColorAPI(Resource):
-    def get(self, color_id):
-        if color_id in myColors:
-            return myColors[color_id].json()
-        else:
-            return Color("No Color Found",0,0,0).json()
+    def put(self, color):
+        GPIO.output(RED_PIN, request.get_json()['red'])
+        GPIO.output(GREEN_PIN, request.get_json()['green'])
+        GPIO.output(BLUE_Pin, request.get_json()['blue'])
+        print("Color Set")
 
-    def put(self, color_id):
-        myColors[color_id] = Color()
-        myColors[color_id].name = color_id
-        myColors[color_id].red = request.get_json()['red']
-        myColors[color_id].green = request.get_json()['green']
-        myColors[color_id].blue = request.get_json()['blue']
-        print myColors[color_id].json()
-        return myColors[color_id].json()
-
-api.add_resource(ColorAPI, '/<string:color_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
