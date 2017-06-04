777c1caa1220d0883f6054d59fdd77999cd92ab6
brodyzt <brodyzt@gmail.com>
2017-06-04 00:36:32 -0400

    Added Color Saving

--- app.py
+++ app.py
@@ -20,26 +20,34 @@ api = Api(app)
 
 mode = "static"
 
+
+last_color = {RED_PIN: 0,
+              GREEN_PIN:0,
+              BLUE_PIN:0 }
+
+def setPins(red, green, blue):
+    pi.set_PWM_dutycycle(RED_PIN, red)
+    pi.set_PWM_dutycycle(GREEN_PIN, green)
+    pi.set_PWM_dutycycle(BLUE_PIN, blue)
+    global  last_color
+    if red != 0 | green != 0 | blue != 0:
+        last_color[RED_PIN] = red
+        last_color[GREEN_PIN] = green
+        last_color[BLUE_PIN] = blue
+
+
 def setPin(pin, brightness):
     pi.set_PWM_dutycycle(pin, brightness)
+    global last_color
+    last_color[pin] = brightness
 
-class Color():
-    def __init__(self, name="", red=0, green=0, blue=0):
-        self.name = name
-        self.red = str(red)
-        self.green = str(green)
-        self.blue = str(blue)
 
-myColor = Color("color", 100, 100, 100)
-myColors = {"color": myColor}
 
 @app.route("/setColor", methods=['PUT'])
 def put():
     global mode
     mode = "static"
-    setPin(RED_PIN, request.get_json()['red'])
-    setPin(GREEN_PIN, request.get_json()['green'])
-    setPin(BLUE_PIN, request.get_json()['blue'])
+    setPins(request.get_json()['red'],request.get_json()['green'],request.get_json()['blue'])
     print("Color Set")
     print(mode)
     return "Complete"
@@ -51,15 +59,11 @@ def flash():
     while mode == "flash":
         print(mode)
         if state == "OFF":
-            setPin(RED_PIN, 255)
-            setPin(GREEN_PIN, 255)
-            setPin(BLUE_PIN, 255)
+            setPins(255, 255, 255)
             state = "ON"
 
         else:
-            setPin(RED_PIN, 0)
-            setPin(GREEN_PIN, 0)
-            setPin(BLUE_PIN, 0)
+            setPins(0, 0, 0)
             state = "OFF"
         time.sleep(FLASH_SPEED)
 
@@ -144,6 +148,21 @@ def execute_update_fade_speed():
 def execute_stop_fade():
     global mode
     mode="static"
+    return "Done"
+
+@app.route("/turnOn", methods=['PUT'])
+def execute_turn_on():
+    global last_color
+    global mode
+    mode="static"
+    setPins(last_color[RED_PIN],last_color[GREEN_PIN],last_color[BLUE_PIN])
+    return "Done"
+
+@app.route("/turnOff", methods=['PUT'])
+def execute_turn_off():
+    global mode
+    mode="static"
+    setPins(0,0,0)
     return "Done"
 
 
